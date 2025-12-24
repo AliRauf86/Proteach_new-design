80424729fed650b3cb0931efb50039f5e2143542
AliRauf86 <rauf_proqweb@mail.ru>
2025-12-24 19:29:29 +0400

    Delete function/api directory

--- function/api/contact.ts
+++
@@ -1,6 +0,0 @@
-export const onRequestPost: PagesFunction = async () => {
-  return new Response(
-    JSON.stringify({ ok: true }),
-    { headers: { "Content-Type": "application/json" } }
-  );
-};
