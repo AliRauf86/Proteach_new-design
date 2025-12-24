4db9c9f5dd27f02072c8b8383dc80d6e54df8034
AliRauf86 <rauf_proqweb@mail.ru>
2025-12-24 19:24:05 +0400

    Add onRequestPost function to contact API

---
+++ functions/api/contact.ts
@@ -0,0 +1,6 @@
+export const onRequestPost: PagesFunction = async () => {
+  return new Response(
+    JSON.stringify({ ok: true }),
+    { headers: { "Content-Type": "application/json" } }
+  );
+};
