403497b491f816a39c4c86b83fae7104555485a2
AliRauf86 <rauf_proqweb@mail.ru>
2025-12-25 01:03:13 +0400

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -23,11 +23,13 @@ const Contact = () => {
       'service_jtazplm',
       'template_s9vdh59',
       {
+        to_email: 'info@proteach.az',
         from_name: formData.name,
         from_email: formData.email,
         phone: formData.phone,
         subject: formData.subject,
         message: formData.message,
+        reply_to: formData.email,
       },
       '4IPEtW6qlCBW7klXl'
     );
