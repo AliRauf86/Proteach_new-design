fc6e3c46c4069a75ae4e827b836bbfc03172db32
AliRauf86 <rauf_proqweb@mail.ru>
2026-01-05 03:18:56 +0400

    Update SolutionsPage.tsx

--- src/pages/SolutionsPage.tsx
+++ src/pages/SolutionsPage.tsx
@@ -1,8 +1,8 @@
-import { Helmet } from "react-helmet-async";
 import { useLanguage } from "@/contexts/LanguageContext";
 import Header from "@/components/Header";
 import Solutions from "@/components/Solutions";
 import Footer from "@/components/Footer";
+import SEO from "@/components/SEO";
 
 const SolutionsPage = () => {
   const { t } = useLanguage();
