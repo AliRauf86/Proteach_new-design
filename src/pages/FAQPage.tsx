d6ae1ea93bcfc8c2d7c77fab425eb58875f1ff32
AliRauf86 <rauf_proqweb@mail.ru>
2026-01-05 03:17:52 +0400

    Update FAQPage.tsx

--- src/pages/FAQPage.tsx
+++ src/pages/FAQPage.tsx
@@ -1,8 +1,8 @@
-import { Helmet } from "react-helmet-async";
 import { useLanguage } from "@/contexts/LanguageContext";
 import Header from "@/components/Header";
 import FAQ from "@/components/FAQ";
 import Footer from "@/components/Footer";
+import SEO from "@/components/SEO";
 
 const FAQPage = () => {
   const { t } = useLanguage();
