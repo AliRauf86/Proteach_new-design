0c6942c9d41aa5c0833a0e4f824c242c5f864880
AliRauf86 <rauf_proqweb@mail.ru>
2026-01-05 03:15:48 +0400

    Update CertificationCriteriaPage.tsx

--- src/pages/CertificationCriteriaPage.tsx
+++ src/pages/CertificationCriteriaPage.tsx
@@ -1,8 +1,8 @@
-import { Helmet } from "react-helmet-async";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import CertificationCriteria from "@/components/CertificationCriteria";
 import { useLanguage } from "@/contexts/LanguageContext";
+import SEO from "@/components/SEO";
 
 const CertificationCriteriaPage = () => {
   const { t } = useLanguage();
