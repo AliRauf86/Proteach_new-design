b7d445ae596d350a25fcc9f99188e6f9eb2453be
AliRauf86 <rauf_proqweb@mail.ru>
2026-01-07 04:07:17 +0400

    Update DeliveryMethodsPage.tsx

--- src/pages/DeliveryMethodsPage.tsx
+++ src/pages/DeliveryMethodsPage.tsx
@@ -27,7 +27,7 @@ const DeliveryMethodsPage = () => {
     { 
       position: 1, 
       name: t("Ana Səhifə", "Home"), 
-      item: "https://proteach.az" 
+      item: "https://proteach.az/" 
     },
     { 
       position: 2, 
