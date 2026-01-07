be51185c055042b02262c80ceefd75f0590919f4
AliRauf86 <rauf_proqweb@mail.ru>
2026-01-07 04:06:55 +0400

    Update ContactPage.tsx

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -24,7 +24,7 @@ const ContactPage = () => {
     { 
       position: 1, 
       name: t("Ana Səhifə", "Home"), 
-      item: "https://proteach.az" 
+      item: "https://proteach.az/" 
     },
     { 
       position: 2, 
